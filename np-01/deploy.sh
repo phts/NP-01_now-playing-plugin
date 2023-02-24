ba445f6e8ed43b8e8ea464f99ad50fc8df124201
Phil Tsaryk <phil@tsaryk.com>
2023-02-24 22:12:15 +0100

    chore: deploy script

---
+++ np-01/deploy.sh
@@ -0,0 +1,9 @@
+#/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+scp ${SCRIPT_DIR}/../app/client/build/static/css/*.css \
+  volumio:/data/plugins/user_interface/now_playing/app/client/build/static/css/
+
+# scp ${SCRIPT_DIR}/../app/client/build/static/js/main.eb06c3dd.js \
+#   volumio:/data/plugins/user_interface/now_playing/app/client/build/static/js/
