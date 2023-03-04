87733d6e7092efd72afbb5a69d979368c9ca61a5
Phil Tsaryk <phil@tsaryk.com>
2023-03-04 17:45:40 +0100

    chore: support deploy script on windows

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,9 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-scp ${SCRIPT_DIR}/../app/client/build/static/css/*.css \
+scp "${SCRIPT_DIR}/../app/client/build/static/css/main.8cb58bff.css" \
   volumio:/data/plugins/user_interface/now_playing/app/client/build/static/css/
-
-scp ${SCRIPT_DIR}/../app/client/build/static/js/main.eb06c3dd.js \
+scp "${SCRIPT_DIR}/../app/client/build/static/css/np-01.css" \
+  volumio:/data/plugins/user_interface/now_playing/app/client/build/static/css/
+scp "${SCRIPT_DIR}/../app/client/build/static/js/main.eb06c3dd.js" \
   volumio:/data/plugins/user_interface/now_playing/app/client/build/static/js/
